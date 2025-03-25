60cb89360770bf4aaa9bc4a902afd80db086e2d1
TheTureFADED <ztx4799@gmail.com>
2025-03-25 00:20:15 -0700

    test

---
+++ MVC/models/config/config.js
@@ -0,0 +1,8 @@
+(() => {
+    const config = {}
+    config.SERVER = 'cluster0.suk4a.mongodb.net'
+    config.USERNAME = 'Faded'
+    config.PASSOWRD = 'AFAllen4799'
+    config.DATABASE = 'Cluster0'
+    module.exports = config
+})()
